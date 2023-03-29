801d8343dbfa8139fff7db8f237ba82f78168fd7
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-03-29 13:02:34 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+idade = input("Digite um numero: ")
+idade = int(idade)
+
+if idade < 0:
+    print("impossivel!")
+
+elif idade < 18:
+    print("não precisa se alistar")
+    
+elif idade > 18 and idade < 65:
+    print("Não esqueça de votar na próxima eleição.")
+    
+elif idade > 65:
+    print("Vá descansar.")
+    
+else:
+    print("eita!")
