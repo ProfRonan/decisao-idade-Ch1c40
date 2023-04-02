e0041cc91442d90b6bba480d95ae0a659e1c9632
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-04-02 09:20:39 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ idade = input("Digite um numero: ")
 idade = int(idade)
 
 if idade < 0:
-    print("impossivel!")
+    print("impossível!")
 
 elif idade < 18:
-    print("não precisa se alistar")
+    print("não precisa se alistar.")
     
 elif idade > 18 and idade < 65:
     print("Não esqueça de votar na próxima eleição.")
